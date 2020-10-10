5308bf877ba4e4ce34ce9849a6c0e094cb9151b6
Bhimashankar <bhimamalbhage@hotmail.com>
2020-10-10 13:41:38 +0530

    Updates

---
+++ precache-manifest.60452e093988feff73e3d13db17d1641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f70ec6660b9398cc892cc7d9ce2556",
+    "url": "/fotonicia-ui/index.html"
+  },
+  {
+    "revision": "4875459c8e819bcd1896",
+    "url": "/fotonicia-ui/static/css/main.f163a27a.chunk.css"
+  },
+  {
+    "revision": "a095dd803ba7003cefc4",
+    "url": "/fotonicia-ui/static/js/2.7f1a1956.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/fotonicia-ui/static/js/2.7f1a1956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4875459c8e819bcd1896",
+    "url": "/fotonicia-ui/static/js/main.f08d7812.chunk.js"
+  },
+  {
+    "revision": "e7984098b85e81d43aa5",
+    "url": "/fotonicia-ui/static/js/runtime-main.9cedfd59.js"
+  }
+]);
